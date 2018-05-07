051164d2d6c0e8fc2a11747d033c1815bad94b74
andrew <a@a-designer.ca>
2018-05-07 02:55:56 -0400

    chaptar 1 ok

--- src/app.ts
+++ src/app.ts
@@ -11,38 +11,36 @@ const reducers = {
   todos: fromStore.reducer
 };
 
-
-
 const store = new fromStore.Store(reducers);
-
-
-console.log(store.value);
 
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const todo = { label: input.value, complete: false };
 
-    console.log(payload);
-
-    store.dispatch({
-      type: 'ADD_TODO',
-      payload
-    });
-
-    console.log(store.value);
-
+    store.dispatch(new fromStore.AddTodo(todo));
 
     input.value = '';
   },
   false
 );
 
+
+const unsubscribe = store.subscribe(state => {
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener('click', unsubscribe, false);
+
+
 todoList.addEventListener('click', function (event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
+    const todo = JSON.parse(target.getAttribute('data-todo') as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
+
+store.subscribe(state => console.log('STATE >>>', state));
